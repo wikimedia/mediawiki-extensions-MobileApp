0a59061fa16fcd31cc606e3c344f2dd8653e06e0
ananay <i@ananayarora.com>
2016-01-07 01:04:54 +0530

    Convert MobileApp extension to use extension.json
    Bug: T88056
    Change-Id: Ife8101919ebcb168addfb3b43885a696751b7391

--- MobileApp.php
+++ MobileApp.php
@@ -8,72 +8,16 @@
  * @copyright © 2014 Yuvi Panda
  * @licence GNU General Public Licence 2.0 or later
  */
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
-	die( -1 );
+
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MobileApp' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['MobileApp'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for Gadgets extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+} else {
+	die( 'This version of the MobileApp extension requires MediaWiki 1.25+' );
 }
 
-$localBasePath = __DIR__;
-$remoteExtPath = 'MobileApp';
-
-// Extension credits that will show up on Special:Version
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'MobileApp',
-	'author' => array( 'Yuvi Panda' ),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:MobileApp',
-	'descriptionmsg' => 'mobileapp-desc',
-	'license-name' => 'GPL-2.0+',
-);
-
-$wgMessagesDirs['MobileApp'] = __DIR__ . "/i18n";
-
-$wgAutoloadClasses['MobileAppResourceLoaderModule'] = __DIR__ . '/MobileAppResourceLoaderModule.php';
-$wgAutoloadClasses['MobileAppHooks'] = __DIR__ . '/MobileApp.hooks.php';
-
-$wgHooks['ListDefinedTags'][] = 'MobileAppHooks::onListDefinedTags';
-$wgHooks['ChangeTagsListActive'][] = 'MobileAppHooks::onListDefinedTags';
-$wgHooks['RecentChange_save'][] = 'MobileAppHooks::onRecentChange_save';
-
-$wgResourceModules['mobile.app.site'] = array( 'class' => 'MobileAppResourceLoaderModule' );
-
-$wgCommonMobileAppModuleDef = array(
-	'localBasePath' => $localBasePath,
-	'remoteExtPath' => $remoteExtPath
-);
-
-$wgResourceModules['mobile.app.pagestyles.android'] = array(
-		'styles' => array(
-				'styles/android.less',
-				'styles/editlinks.less',
-				'styles/issues.less',
-				'styles/disambig.less',
-				'styles/tables.less',
-				'styles/ipa.less',
-				'styles/enwiki.less',
-				'styles/parsoid.less',
-				'styles/widenImages.less'
-			)
-) + $wgCommonMobileAppModuleDef;
-
-$wgResourceModules['mobile.app.pagestyles.android.night'] = array(
-		'styles' => array(
-				'styles/night.less',
-			)
-) + $wgCommonMobileAppModuleDef;
-
-$wgResourceModules['mobile.app.pagestyles.ios'] = array(
-		'styles' => array(
-				'styles/editlinks.less',
-				'styles/enwiki.less',
-				'styles/issues.less',
-				'styles/disambig.less'
-			)
-) + $wgCommonMobileAppModuleDef;
-
-$wgResourceModules['mobile.app.preview'] = array(
-	'styles' => array(
-			'styles/preview.less',
-			'styles/enwiki.less'
-		)
-) + $wgCommonMobileAppModuleDef;
