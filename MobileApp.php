46de56f79f6b6116005ce6e3ba5184ebf2ebff8c
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 20:52:18 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I734faf2ae8a60bb51ece332c0578acff6885beb5

--- MobileApp.php
+++ MobileApp.php
@@ -13,10 +13,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'MobileApp' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['MobileApp'] = __DIR__ . '/i18n';
-	/* wfWarn(
-		'Deprecated PHP entry point used for Gadgets extension. Please use wfLoadExtension instead, ' .
+	wfWarn(
+		'Deprecated PHP entry point used for MobileApp extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 } else {
 	die( 'This version of the MobileApp extension requires MediaWiki 1.25+' );
 }
