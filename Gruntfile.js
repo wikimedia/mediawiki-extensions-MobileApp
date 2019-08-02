4c076fd82e5f30c9d42cd0e443c31e0a90a76523
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-08-02 15:41:06 +0000

    build: Updating npm dependencies
    * set-value: 2.0.0 → 2.0.1
      * https://npmjs.com/advisories/1012
      * CVE-2019-10747
    * union-value: 1.0.0 → 1.0.1
      * https://npmjs.com/advisories/1012
      * CVE-2019-10747
    * mixin-deep: 1.3.1 → 1.3.2
      * https://npmjs.com/advisories/1013
      * CVE-2019-10746
    * lodash: 4.17.11 → 4.17.15
      * https://npmjs.com/advisories/1065
      * CVE-2019-10744
    
    Additional changes:
    * Enable eslint caching.
    * Enable eslint's reportUnusedDisableDirectives.
    
    Change-Id: I0b061705e0f536c322271edc2c190116a544b66e

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,10 @@ module.exports = function ( grunt ) {
 			all: 'i18n/'
 		},
 		eslint: {
+			options: {
+				cache: true,
+				reportUnusedDisableDirectives: true
+			},
 			all: [
 				'**/*.js',
 				'!node_modules/**',
